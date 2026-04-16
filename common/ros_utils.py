543f1d8a4a2f60be1b3d1348bc282d8736ef855f
moonjongsul <moonjongsul@gmail.com>
2026-04-16 09:13:15 +0900

    [fix]

--- common/ros_utils.py
+++ common/ros_utils.py
@@ -19,14 +19,22 @@ def _publisher(node: Node, topic: str, msg_type):
         10
     )
     
-def convert_joint_states_to_array(msg: JointState):
+def convert_joint_states_msg_to_array(msg: JointState):
     return msg.position
 
-def convert_array_to_joint_states(array, joint_names):
+def convert_array_to_joint_states_msg(array, joint_names):
     msg = JointState()
     msg.header = Header()
     msg.name = joint_names
     msg.position = array
     msg.velocity = [0.0] * len(array)
     msg.effort = [0.0] * len(array)
+    return msg
+
+def convert_float_msg_to_float(msg: Float32):
+    return msg.data
+
+def convert_float_to_float_msg(value: float):
+    msg = Float32()
+    msg.data = value
     return msg
