f70a21ecd768d58895b253c4031ff8085da4d087
moonjongsul <moonjongsul@gmail.com>
2026-04-22 12:32:53 +0900

    [update]

--- scripts/data_upload.py
+++ scripts/data_upload.py
@@ -6,4 +6,4 @@ ds = LeRobotDataset(
     repo_id="moonjongsul/flip_object",
     root="/workspace/m.ax/datasets/flip_object",
 )
-ds.push_to_hub(private=True, tags=["robotics", "franka_fr3"])
+ds.push_to_hub(private=False, tags=["robotics", "franka_fr3"])
